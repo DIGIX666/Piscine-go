25608c7fe459d518fe2c1763d4b63afa380edde9
Thox <theo.dbs6610@.com>
2022-03-21 13:48:16 +0100

    to go

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,1 +1,3 @@
-curl https://zone01normandie.org/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"Thox66\"}}){id}}"}'
+#!/bin/bash
+
+curl -s https://zone01normandie.org/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"Thox66\"}}){id}}"}' | cut -c24-26
