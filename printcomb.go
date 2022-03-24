7227b1ba502d9ba3a77308a459bfdf8e3ab5c57d
Thox <theo.dbs6610@.com>
2022-03-24 15:30:57 +0100

    h

--- printcomb.go
+++ printcomb.go
@@ -3,8 +3,20 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := 0o10; i < 998; i++ {
-		z01.PrintRune(i)
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for k := i + 2; k <= '9'; k++ {
+				if k > j && j > i {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+					if i != '7' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
 	}
 	z01.PrintRune('\n')
 }
