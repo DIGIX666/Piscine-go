ea12c0280f8fda7698bff86608579d2b49fec845
Thox <theo.dbs6610@.com>
2022-03-24 11:30:14 +0100

    h

--- printdigits/isnegative.go
+++ printdigits/isnegative.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func main() {
-	number := 8
+	number := -1
 
-	if number >= -1 {
+	if number <= 0 {
 		z01.PrintRune('T')
 	} else {
 		z01.PrintRune('F')
