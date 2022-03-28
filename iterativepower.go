bd0b2731da7335fb9744c266d1a96c98399e6672
Thox <theo.dbs6610@.com>
2022-03-28 17:17:40 +0200

    l

--- iterativepower.go
+++ iterativepower.go
@@ -3,9 +3,23 @@ package piscine
 func IterativePower(nb int, power int) int {
 	if power < 0 {
 		return 0
-	} else if power == 0 {
-		return 1
 	} else {
-		return IterativePower(nb, power-1) * nb
+		reslt := 1
+		for i := 0; i < power; i++ {
+			reslt *= nb
+		}
+		return reslt
 	}
 }
+
+// package piscine
+
+//func IterativePower(nb int, power int) int {
+//	if power < 0 {
+//		return 0
+//	} else if power == 0 {
+//		return 1
+//	} else {
+//		return IterativePower(nb, power-1) * nb
+//	}
+//}
