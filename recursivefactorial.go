0fe049021ed009a99fd543cffb840c5d3bff8aa7
Thox <theo.dbs6610@.com>
2022-03-28 14:46:03 +0200

    k

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -2,7 +2,7 @@ package piscine
 
 func RecursiveFactorial(nb int) int {
 	if nb > 0 && nb < 2147483647 {
-		return nb + RecursiveFactorial(nb-1)
+		return nb * RecursiveFactorial(nb-1)
 	} else if nb == 0 || nb == 1 {
 		return 1
 	} else {
