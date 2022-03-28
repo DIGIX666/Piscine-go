38adee05dc8250c4cfa71f9235da3803610df4a8
Thox <theo.dbs6610@.com>
2022-03-28 16:31:35 +0200

    k

--- fibonacci.go
+++ fibonacci.go
@@ -3,6 +3,9 @@ package piscine
 func Fibonacci(index int) int {
 	if index <= 1 {
 		return index
+	} else if index < 0 {
+		return -1
+	} else {
+		return Fibonacci(index-1) + Fibonacci(index-2)
 	}
-	return Fibonacci(index-1) + Fibonacci(index-2)
 }
