fe495c755f47eb09b9a0d9c9d4118ce8170a667d
Thox <theo.dbs6610@.com>
2022-03-29 20:35:23 +0200

    l

---
+++ isnumeric.go
@@ -0,0 +1,12 @@
+package piscine 
+
+func IsNumeric(s string) bool  {
+	caract := []rune(s)
+	for i := 0; i < len(caract); i++ {
+	if caract[i] < '0' || caract[i] > '9' {
+				return false
+			}
+		}
+		return true
+	}	
+}
