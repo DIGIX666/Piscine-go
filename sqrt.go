afd2095aeabf4b8c7e810a196792139b145e039b
Thox <theo.dbs6610@.com>
2022-03-29 11:41:39 +0200

    k

--- sqrt.go
+++ sqrt.go
@@ -1,14 +1,36 @@
 package piscine
 
 func Sqrt(nb int) int {
-	if nb <= 0 {
+    if nb > 0 {
+        resultat, sqrt := 1, 0
+        for i := 1; resultat <= nb; i++ {
+            resultat = i * i
+            sqrt++
+            if resultat == nb {
+                return sqrt
+            }
+        }
+        return 0
+    } else {
+        return 0
+    }
+}
+
+
+
+
+
+
+
+
+
+/*if nb <= 0 {
 		return 0
 	} else {
-		for i := 0; i < nb; i++ {
+		for i := 0; i < nb/2; i++ {
 			if i*i == nb {
 				return nb
 			}
 		}
 	}
-	return 1
-}
+	return 0
