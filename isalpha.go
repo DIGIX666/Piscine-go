824d9b29ca6d69c21af5190354cb373d89823739
Thox <theo.dbs6610@.com>
2022-03-29 20:29:14 +0200

    l

--- isalpha.go
+++ isalpha.go
@@ -3,7 +3,7 @@ package piscine
 func IsAlpha(s string) bool {
 	caract := []rune(s)
 	for i := 0; i < len(caract); i++ {
-		if (caract[i] < 0 || caract[i] > 9) && (caract[i] < 'a' || caract[i] > 'z') && (caract[i] < 'A' || caract[i] > 'Z') {
+		if (caract[i] < '0' || caract[i] > '9') && (caract[i] < 'a' || caract[i] > 'z') && (caract[i] < 'A' || caract[i] > 'Z') {
 			return false
 		}
 	}
