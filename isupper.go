1409ff95fb365660f5bb2756d958b6fa24a66ba3
Thox <theo.dbs6610@.com>
2022-03-29 19:37:44 +0200

    l

--- isupper.go
+++ isupper.go
@@ -3,7 +3,7 @@ package piscine
 func IsUpper(s string) bool {
 	caract := []rune(s)
 	for i := 0; i < len(caract); i++ {
-		if caract[i] < 'A' && caract[i] > 'Z' {
+		if caract[i] < 'A' || caract[i] > 'Z' {
 			return false
 		}
 	}
