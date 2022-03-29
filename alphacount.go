f75923d25cad9cdfcc62e1446e9fe87064b79d7a
Thox <theo.dbs6610@.com>
2022-03-29 17:07:03 +0200

    k

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,7 @@ func AlphaCount(s string) int {
 	x := 0
 	str := []rune(s)
 	for i := 0; i <= len(s); i++ {
-		if (str[i]) >= 'a' && (str[i]) <= 'z' || ((str[i]) >= 'A' && (str[i]) <= 'Z') {
+		if ((str[i]) >= 'a' && (str[i]) <= 'z') || ((str[i]) >= 'A' && (str[i]) <= 'Z') {
 			x = x + 1
 		}
 	}
