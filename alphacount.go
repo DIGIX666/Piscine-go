93d26ba312bfa21fdf32091356745e58405d9672
Thox <theo.dbs6610@.com>
2022-03-29 16:55:23 +0200

    k

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(s string) int {
 	x := 0
 	for i := 0; i <= len(s); i++ {
-		if rune(i) >= 'a' && rune(i) <= 'z' || rune(i) >= 'A' && rune(i) <= 'z' {
+		if rune(x) >= 'a' && rune(x) <= 'z' || rune(x) >= 'A' && rune(x) <= 'z' {
 			x = x + 1
 		}
 	}
