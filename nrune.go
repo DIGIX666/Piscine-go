87ffa2a472fb95991ce9915b1c6a3ca071248417
Thox <theo.dbs6610@.com>
2022-03-29 14:00:01 +0200

    l

--- nrune.go
+++ nrune.go
@@ -1,7 +1,18 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	last := []rune(s)
+	if n <= 0 {
+		return 0
+	}
+	nr := []rune(s)
+	caract := 0
+	for i := range s {
+		i++
+		caract++
 
-	return last[len(last)-2]
+	}
+	if caract == 0 || n > caract {
+		return 0
+	}
+	return nr[n-1]
 }
