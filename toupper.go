e1f00938da1674dd7ba599f25eaed707958d570f
Thox <theo.dbs6610@.com>
2022-03-30 16:47:16 +0200

    k

--- toupper.go
+++ toupper.go
@@ -4,10 +4,8 @@ func ToUpper(s string) string {
 	mag := []rune(s)
 	for i := 0; i < len(mag); i++ {
 		if mag[i] >= 'a' && mag[i] <= 'z' {
-			mag[i] = mag[i] - 32
+			mag[i] = mag[i] - 32 // a = 97 et A = 65 , du coup pour faire partir de "a" à "A" on fais 97 - 65 = -32
 		}
 	}
 	return string(mag)
 }
-
-/* -32 table ASCII */
