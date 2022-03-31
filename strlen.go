ca64a04d59bf3f74a0600113078feb1a50b420a9
Thox66 <thox66@noreply.git.zone01normandie.org>
2022-03-31 17:42:14 +0000

    Update 'strlen.go'

--- strlen.go
+++ strlen.go
@@ -1,5 +1,6 @@
 package piscine
 
 func StrLen(s string) int {
-	return len([]rune(s))
+	return len([]rune(s)) // affiche la longueur de rune de s (qui notre mot demandé)
 }
+// len(nom pour utiliser la fonction) = lenght = longueur 
