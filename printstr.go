786e48508ee1bc888a50c9863b1648a3049d0045
Thox66 <thox66@noreply.git.zone01normandie.org>
2022-03-31 17:23:03 +0000

    Update 'printstr.go'

--- printstr.go
+++ printstr.go
@@ -3,7 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	for _, v := range s {
-		z01.PrintRune(v)
+	for _, v := range s {   // v vas aller chercher tout ce qui a dans s 
+		z01.PrintRune(v)   // affiche v 
 	}
 }
