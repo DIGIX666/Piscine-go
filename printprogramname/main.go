e2822a7d8366aa20de9891bb4a65906a027823f7
Thox <theo.dbs6610@.com>
2022-03-31 15:27:38 +0200

    k

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	arg := os.Args
-	prog := []rune(arg[0])
+	prog := []rune(arg[0]) // on converti en rune car string de base | arg[0] pour donner la place du nom de progam qui au début
 	for i := 0; i < len(prog); i++ {
-		if prog[i] == '.' || prog[i] == '/' {
-			i++
+		if prog[i] == '.' || prog[i] == '/' { // la variable prog va chercher la position de i
+			i++ // ingrémente i
 		} else {
 			z01.PrintRune(prog[i])
 		}
