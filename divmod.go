55b90c1f6bc2534aab9a796c10522f10de617627
Thox66 <thox66@noreply.git.zone01normandie.org>
2022-03-31 17:16:23 +0000

    Update 'divmod.go'

--- divmod.go
+++ divmod.go
@@ -1,6 +1,6 @@
 package piscine
 
 func DivMod(a int, b int, div *int, mod *int) {
-	*div = a / b
-	*mod = a % b
+	*div = a / b // on stock le résultat de la division qui est le quotien dans le pointeur div
+	*mod = a % b // on stock le reste de la division grâce au % dans le pointeur mod
 }
