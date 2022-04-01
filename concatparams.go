be9545fa8e09b3399ccf092b9d3b7f36b45eb695
Thox <theo.dbs6610@.com>
2022-04-01 20:06:36 +0200

    l

--- concatparams.go
+++ concatparams.go
@@ -1,4 +1,6 @@
-package piscine
+package main
+
+import "fmt"
 
 func ConcatParams(args []string) string {
 	var resultat string // création de variale en string pour renvoyer le résultat en string
@@ -10,10 +12,15 @@ func ConcatParams(args []string) string {
 	}
 
 	for i, v := range args { // v va faire une copie de tout ce que i à récupéré
-		resultat += v // va récupérer le mot
-		if i != mots {
+		resultat += v    // va récupérer le mot
+		if i != mots-2 { //
 			resultat += "\n" // le résultat égale le mot plus un retour à la ligne tout de suite après
 		}
 	}
 	return resultat // affiche le mot
+}
+
+func main() {
+	test := []string{"Hello", "how", "are", "you?", "go"}
+	fmt.Println(ConcatParams(test))
 }
