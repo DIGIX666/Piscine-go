c500fb8061bebf265711a7da3997a62fc85918d5
Thox66 <thox66@noreply.git.zone01normandie.org>
2022-04-01 17:09:10 +0000

    Delete 'ultimatedivmod.go'

--- ultimatedivmod.go
+++
@@ -1,8 +0,0 @@
-package piscine
-
-func UltimateDivMod(a *int, b *int) {
-	y := *a   // on déclare une variable pointeur a pour l'utiliser sur le reste
-
-	*a = *a / *b 
-	*b = y % *b // grâce à variable créée y qui récupère *a permets de rappeler *a pour faire stocké le reste dans *b
-}
