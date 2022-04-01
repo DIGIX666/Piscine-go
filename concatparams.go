60078454e55bee237d111a93b2d7aceede31496f
Thox <theo.dbs6610@.com>
2022-04-01 19:59:58 +0200

    l

---
+++ concatparams.go
@@ -0,0 +1,19 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	var resultat string // création de variale en string pour renvoyer le résultat en string
+	mots := 0           // utiliser pour la taille des mots
+
+	for i := range args { // va compter les mots stocké dans args et va incrémenter i et mots
+		i++
+		mots++
+	}
+
+	for i, v := range args { // v va faire une copie de tout ce que i à récupéré
+		resultat += v // va récupérer le mot
+		if i != mots {
+			resultat += "\n" // le résultat égale le mot plus un retour à la ligne tout de suite après
+		}
+	}
+	return resultat // affiche le mot
+}
