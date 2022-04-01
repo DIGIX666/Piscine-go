2f7e8f0602a3dd11d57f8160f230c6f62c73af8c
Thox <theo.dbs6610@.com>
2022-04-01 19:03:56 +0200

    k

--- appendrange.go
+++ appendrange.go
@@ -4,7 +4,7 @@ func AppendRange(min, max int) []int {
 	var table []int // déclare un tableau de int
 
 	for i := min; i < max; i++ { // Pour i égale min | i inférieur à max alors i incrémente
-		table = append(table, i) //
+		table = append(table, i)
 	}
 	return table
 }
