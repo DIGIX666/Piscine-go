96591debdd9bafea623978da738827f23ab60372
Thox <theo.dbs6610@.com>
2022-04-01 18:54:28 +0200

    k

---
+++ appendrange.go
@@ -0,0 +1,10 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	var table []int // déclare un tableau de int
+
+	for i := min; i < max; i++ { // Pour i égale min | i inférieur à max alors i incrémente
+		table = append(table, i) //
+	}
+	return table
+}
