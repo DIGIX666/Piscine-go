9b6b8d54b7de9fd986b35e763a9e75bded4767ed
Thox66 <thox66@noreply.git.zone01normandie.org>
2022-04-01 17:15:13 +0000

    Delete 'divmod.go'

--- divmod.go
+++
@@ -1,6 +0,0 @@
-package piscine
-
-func DivMod(a int, b int, div *int, mod *int) {
-	*div = a / b // on stock le résultat de la division qui est le quotien dans le pointeur div
-	*mod = a % b // on stock le reste de la division grâce au % dans le pointeur mod
-}
