ffb9fe04308a89a827f0a01d9a60a22316879b32
Thox <theo.dbs6610@.com>
2022-04-02 19:51:15 +0200

    k

--- concatparams.go
+++ concatparams.go
@@ -2,6 +2,10 @@ package piscine
 
 func ConcatParams(args []string) string {
 	resultat := ""
+	for i := range args { // va compter les mots stocké dans args et va incrémenter i et mots
+		i++
+	}
+
 	for _, v := range args {
 		resultat += "\n" + v
 	}
