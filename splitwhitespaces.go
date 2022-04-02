1d76d25bca26b8fccdf806251a6cb1d9243ee8c2
Thox <theo.dbs6610@.com>
2022-04-02 15:40:41 +0200

    l

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,9 +4,9 @@ func SplitWhiteSpaces(s string) []string {
 	var printstr []string // on créer un tableau printstr qui contient des strings
 	j := 0                // on déclare la variable j
 	for i := 0; i < len(s); i++ {
+		j = i + 1
 		if rune(s[i]) == ' ' { // si la string s de variable s est strictement égale à un espace
-			printstr = append(printstr, s[j:i]) // dans le tableau printstr, append va ranger ce que contient s en séparant les mots
-			j = i + 1                           // i + 1 car avec +2 on supprime la première lettre de tout les mots qui suis le premier et un - 1 ne peux pas être possible car sinon ça affiche la dernière lettre du mot précédent en plus du second mot
+			printstr = append(printstr, s[j:i]) // dans le tableau printstr, append va ranger ce que contient s en séparant les mots                           // i + 1 car avec +2 on supprime la première lettre de tout les mots qui suis le premier et un - 1 ne peux pas être possible car sinon ça affiche la dernière lettre du mot précédent en plus du second mot
 		} else if i == len(s)-1 { // sinon si i est strictement égale à la longueur total de la phrase | -1 pour avoir toute la phrase
 			printstr = append(printstr, s[j:])
 		}
