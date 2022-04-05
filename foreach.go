544d353b18027103ac7f0095ad4557c4bcb2bf53
Thox <theo.dbs6610@.com>
2022-04-05 15:37:05 +0200

    k

--- foreach.go
+++ foreach.go
@@ -1,8 +1,8 @@
 package piscine
 
 func ForEach(f func(int), a []int) {
-	for i := range a {
-		f(a[i])
+	for i := range a { // permet de compter le nombre de int
+		f(a[i]) // afficher le résulat en int
 	}
 }
 
