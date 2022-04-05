8243e6c0aad7211b734e25a9c46cae51bbf441cc
Thox <theo.dbs6610@.com>
2022-04-05 16:11:00 +0200

    l

--- map.go
+++ map.go
@@ -3,7 +3,7 @@ package piscine
 func Map(f func(int) bool, a []int) []bool {
 	size := 0
 	for i := range a {
-		size = i
+		size = i + 1
 	}
 
 	table := make([]bool, size) // on fais une nouvelle tranche pour dire que de dans on va ranger vrai / faux ([]bool) et on lui donne la taille avec size
