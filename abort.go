239d746a3e17d26f00d86e6f2e1461dd7a262610
Thox <theo.dbs6610@.com>
2022-04-07 14:37:18 +0200

    k

---
+++ abort.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Abort(a, b, c, d, e int) int {
+	moy := []int{a, b, c, d, e}
+	count := 5
+	x := 0
+
+	for i := 0; i < count-1; i++ {
+		x = i
+		for j := 1; j < count; j++ {
+			if moy[j] < moy[x] {
+				x = j
+			}
+		}
+		moy[i], moy[x] = moy[x], moy[i]
+	}
+	return moy[2]
+}
