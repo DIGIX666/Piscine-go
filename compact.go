aefca70341818d8e98ee8cb3300674aa8c00cea3
Thox <theo.dbs6610@.com>
2022-04-08 01:15:37 +0200

    l

--- compact.go
+++ compact.go
@@ -1,13 +1,20 @@
 package piscine
 
 func Compact(ptr *[]string) int {
+	x := 0
+	for _, v := range *ptr {
+		if v != "" {
+			x++
+		}
+	}
 	count := 0
-	array := make([]string, count)
+	array := make([]string, x)
 	for _, r := range *ptr {
 		if r != "" {
 			array[count] = r
 			count++
 		}
+		*ptr = array
 	}
-	return count
+	return x
 }
