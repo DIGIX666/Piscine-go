7f7ceb6272ee740e78e8b05fc8f8b1f4d2b0361d
Thox <theo.dbs6610@.com>
2022-04-08 00:30:42 +0200

    l

--- compact.go
+++ compact.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 func Compact(ptr *[]string) int {
 	size := 0
@@ -18,23 +16,4 @@ func Compact(ptr *[]string) int {
 		}
 	}
 	return size
-}
-
-const N = 6
-
-func main() {
-	a := make([]string, N)
-	a[0] = "a"
-	a[2] = "b"
-	a[4] = "c"
-
-	for _, v := range a {
-		fmt.Println(v)
-	}
-
-	fmt.Println("Size after compacting:", Compact(&a))
-
-	for _, v := range a {
-		fmt.Println(v)
-	}
 }
