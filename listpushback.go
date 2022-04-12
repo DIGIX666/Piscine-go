0d2c66e540a9bece131431cf72e8826abbcd5648
Thox <theo.dbs6610@.com>
2022-04-12 14:06:48 +0200

    l

--- listpushback.go
+++ listpushback.go
@@ -6,19 +6,28 @@ type NodeL struct {
 }
 
 type List struct {
-	Head *NodeL
-	Tail *NodeL
+	Head *NodeL // premier noeud
+	Tail *NodeL // dernier noeud
 }
 
 func ListPushBack(l *List, data interface{}) {
-	first := &NodeL{Data: data} //  on initialise un noeud et on met nos données à l'intérieur {....}
+	/*first := &NodeL{Data: data} //  on initialise un noeud et on met nos données à l'intérieur {....}
 	if l.Head == nil {          // l.head correspond au premier noeud
 		l.Head = first
 	} else {
 		x := l.Head
 		for x.Next != nil {
-			x = x.Next // .Next veut dire le noeud suivant | du coup on on dit en partant du premier noeaud tu fais la suite
+			x = x.Next // .Next veut dire le noeud suivant | du coup on  dit en partant du premier noeud tu fais la suite
 		}
 		x.Next = first
+	}
+	*/
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+	} else {
+		l.Tail.Next = n
+		l.Tail = n
 	}
 }
